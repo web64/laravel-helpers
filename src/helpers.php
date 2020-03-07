c166f45e3b304f546d04131c4c865c2a725bb9d6
Web64 <web64@users.noreply.github.com>
2020-03-07 14:47:18 +0100

    formatSizeUnits

--- src/helpers.php
+++ src/helpers.php
@@ -276,3 +276,35 @@ function isActiveRoute($route, $output = "active")
 {
     if (Route::currentRouteName() == $route) return $output;
 }
+
+
+// convert bytes to human readable format
+function formatSizeUnits($bytes)
+{
+    if ($bytes >= 1073741824)
+    {
+        $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+    }
+    elseif ($bytes >= 1048576)
+    {
+        $bytes = number_format($bytes / 1048576, 2) . ' MB';
+    }
+    elseif ($bytes >= 1024)
+    {
+        $bytes = number_format($bytes / 1024, 2) . ' KB';
+    }
+    elseif ($bytes > 1)
+    {
+        $bytes = $bytes . ' bytes';
+    }
+    elseif ($bytes == 1)
+    {
+        $bytes = $bytes . ' byte';
+    }
+    else
+    {
+        $bytes = '0 bytes';
+    }
+
+    return $bytes;
+}
